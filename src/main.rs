e46f664786fbecfdf2df89eedebdbd4bd58973b1
Andreev Alexander <andreevlex.as@gmail.com>
2017-12-09 11:49:48 +0300

    Добавлена настройка ArgRequiredElseHelp

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() {
     let app_m = App::new("v8unpack-rs")
         .version(crate_version!())
         .author(crate_authors!())
+        .setting(clap::AppSettings::ArgRequiredElseHelp)
         .about(
             "\n\t2008 Denis Demidov 2008-03-30\n\t2017 Alexander Andreev\n\
              Unpack, pack, deflate and inflate 1C v8 file (*.cf)",
@@ -34,10 +35,5 @@ fn main() {
     if let Some(vals) = app_m.values_of("parse") {
         let v: Vec<&str> = vals.collect();
         parse(v).unwrap();
-    } else {
-        println!(
-            "{}",
-            "Используйте ключ -h для получения справки"
-        );
     }
 }
