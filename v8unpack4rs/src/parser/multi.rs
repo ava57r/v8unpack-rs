7232d023a593ee14117b1ae1c802e138e6229996
Alexander Andreev <andreevlex.as@gmail.com>
2018-03-24 11:08:17 +0300

    RawData -> V8Elem

--- v8unpack4rs/src/parser/multi.rs
+++ v8unpack4rs/src/parser/multi.rs
@@ -10,24 +10,32 @@ use std::thread::{spawn, JoinHandle};
 use inflate;
 use super::single;
 
-fn start_inflate_thread(rawdata: Receiver<RawData>) -> (Receiver<RawData>, JoinHandle<Result<()>>) {
+fn start_inflate_thread(v8_elems: Receiver<V8Elem>) -> (Receiver<V8Elem>, JoinHandle<Result<()>>) {
     let (sender, receiver) = sync_channel(128);
 
     let handle = spawn(move || {
-        for item in rawdata {
-            let (block_data, v8_elem) = (item.block_data, item.v8_elem);
-            let out_data = match inflate::inflate_bytes(&block_data) {
-                Ok(inf_bytes) => inf_bytes,
-                Err(_) => block_data,
-            };
+        for v8_elem in v8_elems {
+            let mut out_element = v8_elem;
 
-            if sender
-                .send(RawData {
-                    v8_elem: v8_elem,
-                    block_data: out_data,
-                })
-                .is_err()
-            {
+            let mut inf_bytes_tuple = (vec![], false);
+
+            if let Some(block_data) = out_element.get_data() {
+                match inflate::inflate_bytes(&block_data) {
+                    Ok(bytes) => {
+                        inf_bytes_tuple.0 = bytes;
+                        inf_bytes_tuple.1 = true;
+                    }
+                    Err(_) => {
+                        inf_bytes_tuple.1 = false;
+                    }
+                }
+            }
+
+            if inf_bytes_tuple.1 == true {
+                out_element = out_element.with_data(inf_bytes_tuple.0);
+            }
+
+            if sender.send(out_element).is_err() {
                 break;
             }
         }
@@ -38,19 +46,22 @@ fn start_inflate_thread(rawdata: Receiver<RawData>) -> (Receiver<RawData>, JoinH
 }
 
 fn start_file_parse(
-    data: Receiver<RawData>,
+    v8_elems: Receiver<V8Elem>,
     p_dir: &path::Path,
     bool_inflate: bool,
 ) -> Result<bool> {
-    for item in data {
-        let (out_data, v8_elem) = (item.block_data, item.v8_elem);
+    for v8_elem in v8_elems {
         let elem_path = p_dir.join(&v8_elem.get_name()?);
 
-        let mut rdr = Cursor::new(&out_data);
-        if rdr.is_v8file() {
-            single::load_file(&mut rdr, bool_inflate)?.save_file_to_folder(&elem_path)?;
+        if let Some(out_data) = v8_elem.get_data() {
+            let mut rdr = Cursor::new(&out_data);
+            if rdr.is_v8file() {
+                single::load_file(&mut rdr, bool_inflate)?.save_file_to_folder(&elem_path)?;
+            } else {
+                fs::File::create(elem_path.as_path())?.write_all(&out_data)?;
+            }
         } else {
-            fs::File::create(elem_path.as_path())?.write_all(&out_data)?;
+            return Ok(false);
         }
     }
     Ok(true)
@@ -63,8 +74,8 @@ pub fn parse_to_folder(file_name: &str, dir_name: &str, bool_inflate: bool) -> R
     };
 
     let (_, elems_addrs) = read_content(file_name)?;
-    let (rawdata, h1) = start_file_reader_thread(path::PathBuf::from(file_name), elems_addrs);
-    let (inf_data, h2) = start_inflate_thread(rawdata);
+    let (v8_elems, h1) = start_file_reader_thread(path::PathBuf::from(file_name), elems_addrs);
+    let (inf_data, h2) = start_inflate_thread(v8_elems);
 
     let result = start_file_parse(inf_data, p_dir, bool_inflate);
 
@@ -80,7 +91,7 @@ pub fn parse_to_folder(file_name: &str, dir_name: &str, bool_inflate: bool) -> R
 fn start_file_reader_thread(
     file_name: path::PathBuf,
     elems_addrs: Vec<ElemAddr>,
-) -> (Receiver<RawData>, JoinHandle<Result<()>>) {
+) -> (Receiver<V8Elem>, JoinHandle<Result<()>>) {
     let (sender, receiver) = sync_channel(128);
 
     let handle = spawn(move || {
@@ -99,24 +110,19 @@ fn start_file_reader_thread(
             }
 
             let elem_block_data = single::read_block_data(&mut buf_reader, &elem_block_header)?;
-            let v8_elem = V8Elem::new().with_header(elem_block_data);
-
-            let mut block_data = vec![];
+            let mut v8_elem = V8Elem::new().with_header(elem_block_data);
 
             if cur_elem.elem_data_addr != V8_MAGIC_NUMBER {
                 buf_reader.seek(SeekFrom::Start(cur_elem.elem_data_addr as u64))?;
                 let block_header_data = BlockHeader::from_raw_parts(&mut buf_reader)?;
 
-                block_data = single::read_block_data(&mut buf_reader, &block_header_data)?;
+                v8_elem = v8_elem.with_data(single::read_block_data(
+                    &mut buf_reader,
+                    &block_header_data,
+                )?);
             }
 
-            if sender
-                .send(RawData {
-                    v8_elem,
-                    block_data,
-                })
-                .is_err()
-            {
+            if sender.send(v8_elem).is_err() {
                 break;
             }
         }
@@ -126,20 +132,17 @@ fn start_file_reader_thread(
     (receiver, handle)
 }
 
-fn start_file_write(rawdata: Receiver<RawData>, p_dir: &path::Path) -> Result<bool> {
-    for item in rawdata {
-        let elem_name = item.v8_elem.get_name()?;
+fn start_file_write(v8_elems: Receiver<V8Elem>, p_dir: &path::Path) -> Result<bool> {
+    for v8_elem in v8_elems {
+        let elem_name = v8_elem.get_name()?;
 
-        let mut file_elem_header = String::new();
-        file_elem_header.push_str(&elem_name);
-        file_elem_header.push_str(".header");
+        let file_elem_header = format!("{0}.{1}", elem_name, "header");
+        fs::File::create(p_dir.join(&file_elem_header))?.write_all(&v8_elem.get_header())?;
 
-        fs::File::create(p_dir.join(&file_elem_header))?.write_all(&item.v8_elem.get_header())?;
-
-        let mut file_elem_data = String::new();
-        file_elem_data.push_str(&elem_name);
-        file_elem_data.push_str(".data");
-        fs::File::create(p_dir.join(&file_elem_data))?.write_all(&item.block_data)?;
+        let file_elem_data = format!("{0}.{1}", elem_name, "data");
+        if let Some(block_data) = v8_elem.get_data() {
+            fs::File::create(p_dir.join(&file_elem_data))?.write_all(block_data)?;
+        }
     }
 
     Ok(true)
@@ -154,9 +157,9 @@ pub fn unpack_pipeline(file_name: &str, dir_name: &str) -> Result<bool> {
     let (file_header, elems_addrs) = read_content(file_name)?;
     fs::File::create(p_dir.join("FileHeader"))?.write_all(&file_header.into_bytes()?)?;
 
-    let (rawdata, h1) = start_file_reader_thread(path::PathBuf::from(file_name), elems_addrs);
+    let (v8_elems, h1) = start_file_reader_thread(path::PathBuf::from(file_name), elems_addrs);
 
-    let result = start_file_write(rawdata, p_dir);
+    let result = start_file_write(v8_elems, p_dir);
 
     let r1 = h1.join().unwrap();
 
@@ -178,9 +181,4 @@ fn read_content(file_name: &str) -> Result<(FileHeader, Vec<ElemAddr>)> {
     let elems_addrs = single::read_elems_addrs(&mut buf_reader, &first_block_header)?;
 
     Ok((file_header, elems_addrs))
-}
-
-struct RawData {
-    v8_elem: V8Elem,
-    block_data: Vec<u8>,
 }
